21bdcd1f518c30eff5975dbbd3f285a2b9602818
Taylor Moore <github@taylormoore.ca>
2019-04-21 12:48:56 -0600

    Included a chart class

--- KSP/KSP.py
+++ KSP/KSP.py
@@ -1,6 +1,7 @@
 ﻿from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from PyQt5.QtChart import *
 import krpc
 import time
 
@@ -55,15 +56,6 @@ class KrpcClient(QThread):
                     self.statusLabel_setText_trigger.emit("Flying")
                     self.launchPushButton_setEnabled_trigger.emit(False)
                     # Check next stage
-                    #fuel_amount = self.conn.get_call(self.vessel.resources.amount, 'SolidFuel')
-                    #expr = self.conn.krpc.Expression.less_than(
-                    #    self.conn.krpc.Expression.call(fuel_amount),
-                    #    self.conn.krpc.Expression.constant_float(0.5))
-                    #event = self.conn.krpc.add_event(expr)
-                    #with event.condition:
-                    #    event.wait()
-                    #print('Booster separation')
-                    #self.vessel.control.activate_next_stage()
                     while stage is 0:
                         fuel_amount = self.conn.get_call(self.vessel.resources.amount, "Solid Fuel")
                         expr = self.conn.krpc.Expression.less_than(self.conn.krpc.Expression.call(fuel_amount), self.conn.krpc.Expression.constant_float(0.1))
@@ -75,10 +67,75 @@ class KrpcClient(QThread):
                         stage += 1
                     while stage is 1:
                         time.sleep(1)
+                        tick = 0
+                        #altitudeData.append(tick, 3)
+                        #altitudeChart.addSeries(altitudeData)
+                        #altitudeChart.createDefaultAxes()
 
 
     def __del__(self):
         self.exit()
+
+    #chart = QChart()
+    #chart.legend().hide()
+    #chartView = QChartView(chart)
+
+    #chartView.setRenderHint(QPainter.Antialiasing)
+    #layout.addWidget(chartView)
+
+    #altitudeData = QLineSeries()
+    #pen = altitudeData.pen()
+    #pen.setWidthF(1)
+    #altitudeData.setPen(pen)
+    #altitudeData.setUseOpenGL(True)
+    #altitudeData.append(1,1)
+    #altitudeData.append(2,3)
+    #chart.addSeries(altitudeData)
+    #altitudeData.append(4,0)
+    #axisX = QValueAxis()
+    #axisX.setRange(0,5)
+    #axisX.setTickCount(5)
+    #chart.setAxisX(axisX)
+    #altitudeData.attachAxis(axisX)
+    #axisY = QValueAxis()
+    #axisY.setRange(0,5)
+    #axisY.setTickCount(5)
+    #chart.setAxisY(axisY)
+    #altitudeData.attachAxis(axisY)
+    ##altitudeChart.createDefaultAxes()
+    ##altitudeChartView.repaint()
+    ##altitudeChart.update()
+
+
+class Chart(QObject):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.chart = QChart()
+        self.chartView = QChartView(self.chart)
+        self.chart.legend().hide()
+        self.chartView.setRenderHint(QPainter.Antialiasing)
+        layout.addWidget(self.chartView)
+
+        self.dataSeries = QLineSeries()
+        pen = self.dataSeries.pen()
+        pen.setWidth(1)
+        self.dataSeries.setPen(pen)
+        self.dataSeries.setUseOpenGL(True)
+        self.chart.addSeries(self.dataSeries)
+        self.axisX = QValueAxis()
+        self.axisX.setRange(0,5)
+        self.axisX.setTickCount(5)
+        self.chart.setAxisX(self.axisX)
+        self.dataSeries.attachAxis(self.axisX)
+        self.axisY = QValueAxis()
+        self.axisY.setRange(0,5)
+        self.axisY.setTickCount(5)
+        self.chart.setAxisY(self.axisY)
+        self.dataSeries.attachAxis(self.axisY)
+
+    def addData(self, x, y):
+        self.dataSeries.append(x,y)
+
 
 # Qt callbacks
 def on_launchPushbutton_clicked():
@@ -88,6 +145,7 @@ def on_launchPushbutton_clicked():
 if __name__ == "__main__":
     app = QApplication([])
     window = QWidget()
+    window.setMinimumSize(800, 500)
     layout = QVBoxLayout()
 
     statusLabel = QLabel()
@@ -98,8 +156,13 @@ if __name__ == "__main__":
     launchPushbutton.clicked.connect(on_launchPushbutton_clicked)
     layout.addWidget(launchPushbutton)
 
-    krpcClient = KrpcClient(statusLabel, launchPushbutton)
-    krpcClient.start()
+    #krpcClient = KrpcClient(statusLabel, launchPushbutton)
+    #krpcClient.start()
+
+    altitudeChart = Chart()
+    altitudeChart.addData(0,1)
+    altitudeChart.addData(1,2)
+    altitudeChart.addData(2,0)
 
     window.setLayout(layout)
     window.show()
